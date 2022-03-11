e9d51d1bab1651e8a25dd1da9c3e4ae10232e695
Gustavo Barreto <gsbarreto95@gmail.com>
2022-03-10 22:33:32 -0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">ToDo Vue</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+            <router-link class="nav-item" to="/">Tarefas</router-link> |
+            <router-link class="nav-item" to="/form">Formulario</router-link>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+};
+</script>
+
+<style scoped>
+.nav-item{
+    color: #fff;
+    margin: 0 0.5rem;
+    text-decoration: none;
+}
+</style>
---
+++ src/views/Form.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mt-2">
+    <FormTasks />
+  </div>
+</template>
+
+<script>
+import FormTasks from "../components/FormTasks.vue";
+export default {
+  components: {
+    FormTasks,
+  },
+};
+</script>
